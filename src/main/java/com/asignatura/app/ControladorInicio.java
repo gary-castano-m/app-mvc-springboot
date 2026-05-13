819d54a609a83e384b54b04d55d2aa62b561c29d
gcastanom <gcastano@unicartagena.edu.co>
2026-05-12 21:25:38 -0500

    replica guia 05-Intercambio de datos

--- src/main/java/com/asignatura/app/ControladorInicio.java
+++ src/main/java/com/asignatura/app/ControladorInicio.java
@@ -1,15 +1,22 @@
 package com.asignatura.app;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @Slf4j
 public class ControladorInicio {
+    @Value ("${index.mensaje}")
+    String dato;
     @GetMapping("/")
-    public String inicio() {
+    public String inicio(Model modelo) {
+        String mensaje = "Saludos desde Spring MVC";
+        modelo.addAttribute ("mensaje", mensaje);
+        modelo.addAttribute ("dato", dato);
         log.info("Ejecutando el contralodor Inicio");
         return "index";
     }
