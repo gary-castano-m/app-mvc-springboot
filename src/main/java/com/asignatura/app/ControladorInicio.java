c25bb92f7a8f77dfa105a91fbffb22e72e236f75
gcastanom <gcastano@unicartagena.edu.co>
2026-05-12 22:31:19 -0500

    replica guia 06-Intercambio de Objetos

--- src/main/java/com/asignatura/app/ControladorInicio.java
+++ src/main/java/com/asignatura/app/ControladorInicio.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.asignatura.app.modelo.Usuario;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -17,6 +18,12 @@ public class ControladorInicio {
         String mensaje = "Saludos desde Spring MVC";
         modelo.addAttribute ("mensaje", mensaje);
         modelo.addAttribute ("dato", dato);
+        Usuario u = new Usuario ();
+        u.setCedula("123456789");
+        u.setClave("Abcd");
+        u.setNombre("GARY JOSE CASTAÑO MOLINA");
+        u.setEmail("gcastanom@unicartagena.edu.co");
+        modelo.addAttribute ("alguien", u);
         log.info("Ejecutando el contralodor Inicio");
         return "index";
     }
---
+++ src/main/java/com/asignatura/app/modelo/Usuario.java
@@ -0,0 +1,13 @@
+package com.asignatura.app.modelo;
+
+
+import lombok.Data;
+
+@Data
+public class Usuario {
+    //  PROPIEDADES
+    private String cedula;
+    private  String clave;
+    private  String nombre;
+    private String email;
+}
