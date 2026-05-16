d4c3ea3da3a1458d4f02dd2bba34dfe85556bd2c
gcastanom <gcastano@unicartagena.edu.co>
2026-05-16 16:19:19 -0500

    replica guia 07-Intercambio lista de objetos

--- src/main/java/com/asignatura/app/ControladorInicio.java
+++ src/main/java/com/asignatura/app/ControladorInicio.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.asignatura.app.modelo.Usuario;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -18,12 +20,24 @@ public class ControladorInicio {
         String mensaje = "Saludos desde Spring MVC";
         modelo.addAttribute ("mensaje", mensaje);
         modelo.addAttribute ("dato", dato);
-        Usuario u = new Usuario ();
-        u.setCedula("123456789");
-        u.setClave("Abcd");
-        u.setNombre("GARY JOSE CASTAÑO MOLINA");
-        u.setEmail("gcastanom@unicartagena.edu.co");
-        modelo.addAttribute ("alguien", u);
+        Usuario u1 = new Usuario ();
+        u1.setCedula("123456789");
+        u1.setClave("Abcd");
+        u1.setNombre("GARY JOSE CASTAÑO MOLINA");
+        u1.setEmail("gcastanom@unicartagena.edu.co");
+        modelo.addAttribute ("alguien", u1);
+        Usuario u2 = new Usuario ();
+        u2.setCedula("45549964");
+        u2.setClave("Vaya");
+        u2.setNombre("YURIS MARCELA TOVAR DEL CASTILLO");
+        u2.setEmail("cami041002@hotmail.com");
+        Usuario u3 = new Usuario ();
+        u3.setCedula("45471861");
+        u3.setClave("Jack");
+        u3.setNombre("JACKELIN MOLINA BELTRAN");
+        u3.setEmail("jamobe27@hotmail.com");
+        List<Usuario> listaUsuarios = Arrays.asList(u2, u3);
+        modelo.addAttribute ("usuarios", listaUsuarios);
         log.info("Ejecutando el contralodor Inicio");
         return "index";
     }
