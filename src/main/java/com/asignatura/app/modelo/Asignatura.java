6959e690bcdb6426fe5a2222fda3733436cdab9f
gcastanom <gcastano@unicartagena.edu.co>
2026-05-30 19:47:42 -0500

    Correcccion de errores-Alineacion de Nomenclatura

--- src/main/java/com/asignatura/app/modelo/Asignatura.java
+++ src/main/java/com/asignatura/app/modelo/Asignatura.java
@@ -1,18 +1,21 @@
 package com.asignatura.app.modelo;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-import java.sql.Timestamp;
-import java.time.Instant;
 
 @Entity
 @Table(name = "asignatura")
 @Data
 public class Asignatura implements Serializable {
+
     @Id
     @Basic(optional = false)
     @Column(name = "id", nullable = false, length = 36)
@@ -47,28 +50,4 @@ public class Asignatura implements Serializable {
 
     @Column(name = "profesor", length = 100)
     private String profesor;
-
-    @Column(name = "created_at", nullable = true, updatable = false)
-    Timestamp created_at;
-
-    @Column(name = "updated_at", nullable = true, updatable = true)
-    Timestamp updated_at;
-
-    @PrePersist
-    public void prePersist() {
-        Timestamp now = Timestamp.from(Instant.now());
-
-        if (created_at == null) {
-            created_at = now;
-        }
-
-        if (updated_at == null) {
-            updated_at = now;
-        }
-    }
-
-    @PreUpdate
-    public void preUpdate() {
-        updated_at = Timestamp.from(Instant.now());
-    }
 }
